01fc4a8005cbbf7f0085ac9172ea885e622e729b
Kevinptl23 <kevinptl98@gmail.com>
2025-12-22 23:59:23 +0530

    Initial commit - add frontend and backend folders

---
+++ server/database/db.js
@@ -0,0 +1,13 @@
+import mongoose, { mongo } from "mongoose";
+
+const dbConnection = () => {
+    mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to DB!");
+    })
+    .catch((e) => {
+        console.log("Error in connection to database : ", e);
+    })
+}
+
+export default dbConnection;
